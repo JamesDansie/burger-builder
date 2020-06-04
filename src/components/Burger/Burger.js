870d916acc65b418ba3d795b6708336cb70b6e85
JamesDansie <jdansie8336@gmail.com>
2020-06-04 16:18:02 -0700

    added burger state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,24 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    // so what is this doing?
+    // first getting the keys from the ingredients object
+    // then mapping it to an array
+    // spreading that array and making entries equal to
+    // the value for the key.
+    // then mapping the ingredient/key that many times.
+    // so bacon: 2 becomes [bacon, bacon]
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            })
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
-
         </div>
     );
 }
