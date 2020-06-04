8b4549c68aa7fa963efe5b7edd10cf702cfcaea7
JamesDansie <jdansie8336@gmail.com>
2020-06-04 16:45:50 -0700

    added burger builder controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -11,12 +11,22 @@ const burger = (props) => {
     // the value for the key.
     // then mapping the ingredient/key that many times.
     // so bacon: 2 becomes [bacon, bacon]
-    const transformedIngredients = Object.keys(props.ingredients)
+    // it then flattens the array with reduce, so instead
+    // of an array of arrays, it's just an array.
+    // god js is ugly
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             })
-        });
+        })
+        .reduce((arr, ele) => {
+            return arr.concat(ele)
+        }, []);
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add some ingredients</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
