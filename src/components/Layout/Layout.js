5c596c4b0c2335d188e4b6a90ff244c7af1d7f51
JamesDansie <jdansie8336@gmail.com>
2020-06-08 11:58:06 -0700

    much fusing with side bar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import styles from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <main className={styles.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSiderDrawer: true
+    }
+    
+    sideDrawerClosedHandler = () => {
+        this.setState({showSiderDrawer: false})
+    }
 
-export default layout;
+    render() {
+        return(
+            <Aux>
+                <Toolbar />
+                <SideDrawer open={this.state.showSiderDrawer} closed={this.sideDrawerClosedHandler}/>
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
