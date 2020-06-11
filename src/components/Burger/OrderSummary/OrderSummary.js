97258c23b72ddcdc5a9a14c70396ed383bc2599d
JamesDansie <jdansie8336@gmail.com>
2020-06-11 14:18:15 -0700

    make order summary and modal into classes to add llife cycles

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    // if we get rid of WillUpdate then we
+    // can change back to a functional component
+    componentWillUpdate() {
+        console.log('[OrderSummary] Willupdate');
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
             <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
             </li>);
         });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A tasty burger with these ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Success" clicked={props.purchaseContinue}>Continue</Button>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-        </Aux>
-    );
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A tasty burger with these ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Success" clicked={this.props.purchaseContinue}>Continue</Button>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+            </Aux>
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
